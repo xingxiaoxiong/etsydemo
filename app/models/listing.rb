6d6b1d9b69ac41ffe5a5273d315cab3c437c617e
Xiaoxiong Xing <xingxiaoxiong@frontier.hokudai.ac.jp>
2014-07-23 21:49:05 +0900

    finish setup dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,12 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
-    #validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/,
-  :storage => :dropbox,
-  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  if Rails.env.development?
+	  has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  else
+    as_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
+    :storage => :dropbox,
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    :path => ":style/:id_:filename"
+    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  end
 end
