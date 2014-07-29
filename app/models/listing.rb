964fcd975450f865860e3ff59314e52d56e154b5
Xiaoxiong Xing <xingxiaoxiong@frontier.hokudai.ac.jp>
2014-07-29 22:33:37 +0900

    Validate users and listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,12 +1,14 @@
 class Listing < ActiveRecord::Base
   if Rails.env.development?
     has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   else
     has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
     :storage => :dropbox,
     :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     :path => ":style/:id_:filename"
-    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   end
+
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0}
 end
