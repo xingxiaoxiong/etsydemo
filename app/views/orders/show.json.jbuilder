869b79d88d8522cf6d23d3b5cfeb1ab702be72c8
Xiaoxiong Xing <xingxiaoxiong@frontier.hokudai.ac.jp>
2014-08-06 20:08:12 +0900

    Enable user to place order

---
+++ app/views/orders/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @order, :id, :address, :city, :created_at, :updated_at
---
+++ db/migrate/20140806084352_create_orders.rb
@@ -0,0 +1,10 @@
+class CreateOrders < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.string :address
+      t.string :city
+
+      t.timestamps
+    end
+  end
+end
